a3f31efc1e246ff44437fc17b45b7af4732f3b0d
Aryan Mehta <aryan@Aryans-MacBook-Air.local>
2024-11-14 22:48:30 -0500

    A5 - Finish Lab 3.4 completely

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
 import Hello from "./Hello.js";
+import Lab5 from './Lab5/index.js';
 
 const app = express();
 app.use(express.json());
 
 Hello(app);
+Lab5(app);
 
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
