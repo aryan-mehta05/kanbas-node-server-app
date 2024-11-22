eaea6f146ffdcbb2d500a5fdd64dc481ef7cc0ae
Aryan Mehta <aryan@Aryans-MacBook-Air.local>
2024-11-22 00:00:31 -0500

    A5 - Complete Labs for A5

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -25,13 +25,15 @@ let todos = [
 const WorkingWithArrays = (app) => {
   // CREATE Operation - From CRUD Operations
   app.get("/lab5/todos/create", (req, res) => {
-    const newTodo = {
-      id: new Date().getTime(),
-      title: "New Task",
-      completed: false,
-    };
+    const newTodo = { id: new Date().getTime(), title: "New Task", completed: false };
     todos.push(newTodo);
     res.json(todos);
+  });
+
+  app.post("/lab5/todos", (req, res) => {
+    const newTodo = { ...req.body,  id: new Date().getTime() };
+    todos.push(newTodo);
+    res.json(newTodo);
   });
 
   // READ Operation - From CRUD Operations
@@ -79,12 +81,40 @@ const WorkingWithArrays = (app) => {
     res.json(todos);
   });
 
+  app.put("/lab5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+    if (todoIndex === -1) {
+      res.status(404).json({ message: `Unable to update Todo with ID ${id}` });
+      return;
+    }
+
+    todos = todos.map((t) => {
+      if (t.id === parseInt(id)) {
+        return { ...t, ...req.body };
+      }
+      return t;
+    });
+    res.sendStatus(200);
+  });
+
   // DELETE Operation - From CRUD Operations
   app.get("/lab5/todos/:id/delete", (req, res) => {
     const { id } = req.params;
     const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
     todos.splice(todoIndex, 1);
     res.json(todos);
+  });
+
+  app.delete("/lab5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+    if (todoIndex === -1) {
+      res.status(404).json({ message: `Unable to delete Todo with ID ${id}` });
+      return;
+    }
+    todos.splice(todoIndex, 1);
+    res.sendStatus(200);
   });
 };
 
